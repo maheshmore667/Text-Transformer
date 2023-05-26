e944a636b465c8a0612b9d756ac6aadea3d16b86
Mahesh More <maheshmore667@gmail.com>
2023-05-26 21:20:43 +0530

    description updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,7 @@ export default function About() {
     <div className="text col-12 text-center my-2">
         <h3>About Me</h3>
         <h5>MEAN && MERN Developer & <span>Technical Author</span></h5>
-          <p>My name is Mahesh More. I am  MEAN and MERN stack developer having total experience of 4+ years.</p>
+          <p>My name is Mahesh More. having total strong experience of 4+ years building Single page application using (MEAN /MERN) stack.</p>
           <p>Sweet and simple intro I guess!!</p>
         <div className="data">
         <Link to="/" className="hire">Hire Me</Link>
